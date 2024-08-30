e675311c8c8265b039423b4997b7195fc8677c51
GangPil Song <92740959+strongfeel@users.noreply.github.com>
2024-08-30 11:14:30 +0900

    to-do 리스트 기본 설정

---
+++ app.js
@@ -0,0 +1,25 @@
+// app.js
+
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+// Express에서 req.body에 접근하여 body 데이터를 사용할 수 있도록 설정합니다.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// static Middleware, express.static()을 사용하여 정적 파일을 제공합니다.
+app.use(express.static("./assets"));
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  return res.json({ message: "Hi!" });
+});
+
+app.use("/api", router);
+
+app.listen(PORT, () => {
+  console.log(PORT, "포트로 서버가 열렸어요!");
+});
